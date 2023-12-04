1df0c90b2e3431c6639e1ea1bc524d0af72ab651
TuckerSelf <tself3@ut.utm.edu>
2023-12-04 11:37:16 -0600

    some basics

--- main.cpp
+++ main.cpp
@@ -18,13 +18,31 @@ void autoEnter(int num);
 
 int main() {
   deque dQ;
-  
+  int userChoice;
+
+  cout << "Welcome to the Deque Toolkit. What operation would you like to do?: " << endl
+       << "Push data to front: 1" << endl << "Push data to back: 2" << endl
+       << "Pop front data: 3" << endl << "Pop back data: 4" << endl << "Insert numbers(from 0 to given): 5"
+       << endl << "Retrieve front data: 6" << endl << "Retrieve back data: 7" << endl
+       << "Output deque size: 8" << endl << "Check if deque is empty: 9" << endl
+       << "Exit Program: 0" << endl << "Your Choice: ";
+  cin >> userChoice;
+
+  userOperation(userChoice);
   
   return 0;
 }
 
 void userOperation(int input){
+  if(input == 1){
 
+  }
+  else if(input == 5){
+    int decision;
+    cout << "How many ints would you like to input?: ";
+    cin >> decision;
+    autoEnter(decision);
+  }
 }
 
 void autoEnter(int num){
